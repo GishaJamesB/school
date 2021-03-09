c63738d42bc64b777396c36f47ca014c1e8a89cc
Gisha James <gisha.james@urbanfox.asia>
2021-03-09 09:22:59 +0800

    deleted unnecessary data from test files

--- test/utils/attendance.exs
+++ test/utils/attendance.exs
@@ -5,33 +5,33 @@ defmodule School.ErrorsTest do
 
   test "merge_attendance_with_child_data - merge attendance property to children's data" do
     all_children = [
-      %{:id => 1, :name => "Joe"},
-      %{:id => 2, :name => "Doe"},
-      %{:id => 3, :name => "Harry"},
-      %{:id => 4, :name => "Potter"}
+      %{:id => 1},
+      %{:id => 2},
+      %{:id => 3},
+      %{:id => 4}
     ]
 
     assert Attendance.merge_attendance_with_child_data(all_children, [1,3]) == [
-      %{:id => 1, :name => "Joe", :attendance => true},
-      %{:id => 2, :name => "Doe", :attendance => false},
-      %{:id => 3, :name => "Harry", :attendance => true},
-      %{:id => 4, :name => "Potter", :attendance => false}
+      %{:id => 1, :attendance => true},
+      %{:id => 2, :attendance => false},
+      %{:id => 3, :attendance => true},
+      %{:id => 4, :attendance => false}
     ]
   end
 
   test "get_attendance_by_child_id_and_date_from_list" do
     data = %{
       ~D[2021-02-21] => [
-        %{attendance: true, id: 1, name: "Joe"},
-        %{attendance: true, id: 2, name: "Doe"},
-        %{attendance: false, id: 26, name: "Harry"},
-        %{attendance: false, id: 21, name: "Potter"},
+        %{attendance: true, id: 1},
+        %{attendance: true, id: 2},
+        %{attendance: false, id: 26},
+        %{attendance: false, id: 21},
       ],
       ~D[2021-02-28] => [
-        %{attendance: false, id: 1, name: "Joe"},
-        %{attendance: false, id: 2, name: "Doe"},
-        %{attendance: true, id: 26, name: "Harry"},
-        %{attendance: true, id: 21, name: "Potter"},
+        %{attendance: false, id: 1},
+        %{attendance: false, id: 2},
+        %{attendance: true, id: 26},
+        %{attendance: true, id: 21},
       ]
     }
 
@@ -41,20 +41,20 @@ defmodule School.ErrorsTest do
   test "get_attendance_for_the_child_for_all_dates" do
     data = %{
       ~D[2021-02-21] => [
-        %{attendance: true, id: 1, name: "Joe"},
-        %{attendance: true, id: 2, name: "Doe"},
-        %{attendance: false, id: 26, name: "Harry"},
-        %{attendance: false, id: 21, name: "Potter"},
+        %{attendance: true, id: 1},
+        %{attendance: true, id: 2},
+        %{attendance: false, id: 26},
+        %{attendance: false, id: 21},
       ],
       ~D[2021-02-28] => [
-        %{attendance: false, id: 1, name: "Joe"},
-        %{attendance: false, id: 2, name: "Doe"},
-        %{attendance: true, id: 26, name: "Harry"},
-        %{attendance: true, id: 21, name: "Potter"},
+        %{attendance: false, id: 1},
+        %{attendance: false, id: 2},
+        %{attendance: true, id: 26},
+        %{attendance: true, id: 21},
       ]
     }
 
-    dates = [%{date: ~D[2021-02-21], id: 1}, %{date: ~D[2021-02-28], id: 2}]
+    dates = [%{date: ~D[2021-02-21]}, %{date: ~D[2021-02-28]}]
 
     assert Attendance.get_attendance_for_the_child_for_all_dates(2,dates, data) == %{
       ~D[2021-02-21] => true,

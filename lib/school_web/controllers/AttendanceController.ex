d6d8e9342a250e6f67306e03318a573d01cc13d8
Gisha James <gisha.james@urbanfox.asia>
2021-03-16 10:31:47 +0800

    refactored alias

--- lib/school_web/controllers/AttendanceController.ex
+++ lib/school_web/controllers/AttendanceController.ex
@@ -1,14 +1,14 @@
 defmodule SchoolWeb.AttendanceController do
   use SchoolWeb, :controller
 
-  alias School.Services.Attendance
+  alias School.Services.Attendance, as: AttendanceService
 
   def create(conn, params) do
     changeset = params |> Map.delete("attendance")
     if(Map.get(params, "attendance") == false) do
-      School.Services.Attendance.delete(changeset)
+      AttendanceService.delete(changeset)
     else
-      case School.Services.Attendance.create(changeset) do
+      case AttendanceService.create(changeset) do
         {:ok, _} -> render conn, "success.json", %{message: "completed"}
         {:error, _} -> render conn, "failure.json", %{message: "could not mark attendance"}
       end
@@ -16,11 +16,11 @@ defmodule SchoolWeb.AttendanceController do
   end
 
   def get_attendance_by_date(conn, %{"date" => date}) do
-    data = Attendance.get_attendance_by_date(date)
+    data = AttendanceService.get_attendance_by_date(date)
     json(conn, %{children: data})
   end
 
   def get_attendance(conn, _params) do
-    json(conn, %{data: Attendance.get_full_attendance})
+    json(conn, %{data: AttendanceService.get_full_attendance})
   end
 end
